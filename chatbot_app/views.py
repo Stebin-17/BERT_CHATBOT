1e03e5d528e3ad2910b6f035fafae0fc75071d75
Stebin George <114398468+Stebin-17@users.noreply.github.com>
2023-07-12 01:10:20 +0530

    last_commit

--- chatbot_app/views.py
+++ chatbot_app/views.py
@@ -1,19 +1,26 @@
+import PyPDF2
+import docx
+import newspaper
+import nltk
+from nltk.tokenize import word_tokenize
+from langdetect import detect, LangDetectException
+from bs4 import BeautifulSoup
 from django.http import HttpResponseBadRequest, JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from transformers import BertTokenizer, BertForQuestionAnswering
-from bs4 import BeautifulSoup
 import torch
 import urllib.request
-import PyPDF2
-import docx
-import re   
-import nltk
-from nltk.tokenize import word_tokenize
-from langdetect import detect
-from langdetect.lang_detect_exception import LangDetectException
+import re
 import requests
-import newspaper
+
+# Load the BERT model and tokenizer
+model_name = "bert-large-uncased-whole-word-masking-finetuned-squad"
+tokenizer = BertTokenizer.from_pretrained(model_name)
+model = BertForQuestionAnswering.from_pretrained(model_name)
+
+# File paths
+context_file_path = 'chatbot_app/context.txt'
 
 # For extracting text from pdf
 def extract_text_from_pdf(file_path):
@@ -40,14 +47,6 @@ def read_context(file_path):
 def write_context(file_path, context):
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(context)
-
-# Load the BERT model and tokenizer
-model_name = "bert-large-uncased-whole-word-masking-finetuned-squad"
-tokenizer = BertTokenizer.from_pretrained(model_name)
-model = BertForQuestionAnswering.from_pretrained(model_name)
-
-
-context_file_path = 'chatbot_app/context.txt'
 
 # Tokenizing the context and answer and making the best answer to sent to the bot_response
 def get_bot_response(question):
@@ -92,125 +91,6 @@ def get_bot_response(question):
 
     return best_answer
 
-
-
-#Api for Bot Interaction as chat
-
-@api_view(['POST'])
-def chatbot(request):
-    user_message = request.data.get('message')
-    if not user_message:
-        return HttpResponseBadRequest("Invalid 'message' parameter.")
-
-    bot_response = get_bot_response(user_message)
-
-    if not bot_response.strip():
-        bot_response = "Sorry, I don't know the answer to that."
-
-    return Response({'message': bot_response})
-
-
-# Api for retrieving the current context 
-
-@api_view(['GET'])
-def get_current_context(request):
-    # Retrieve the current context data from the file
-    context_data = read_context(context_file_path)
-
-    # Format the context data as a JSON response
-    return JsonResponse({'context': context_data})
-
-# Api associated with the updating the current context when user enters the content in the paragraph section to the frontend
-
-@api_view(['POST'])
-def update_context(request):
-    paragraph = request.data.get('paragraph')
-    if not paragraph:
-        return HttpResponseBadRequest("Invalid 'paragraph' parameter.")
-
-    try:
-        # Update the context file with the paragraph
-        write_context(context_file_path, paragraph)
-
-        return Response({'message': 'Context updated successfully.'})
-    except Exception as e:
-        return Response({'message': 'Error updating context: {}'.format(str(e))})
-
-# Api associated with the Upload files button, returns the content of the document to the frontend
-
-@api_view(['POST'])
-def upload_document(request):
-    try:
-        # Retrieve the uploaded document from the request
-        document = request.FILES.get('file')
-        if not document:
-            return HttpResponseBadRequest("No document uploaded.")
-
-        # Save the document using Django's default storage
-        file_path = 'chatbot_app/context.txt'
-        with open(file_path, 'wb') as file:
-            for chunk in document.chunks():
-                file.write(chunk)
-
-        # Extract text content based on the file type
-        text = ''
-        if document.name.endswith('.pdf'):
-            text = extract_text_from_pdf(file_path)
-        elif document.name.endswith('.txt'):
-            text = read_text_file(file_path, encoding='utf-8')  # Specify the appropriate encoding
-        elif document.name.endswith('.docx'):
-            text = extract_text_from_docx(file_path)
-
-        # Data pre-processign for the documents
-        new_text=clean_content(text)
-        # Save the extracted text to the context file
-        write_context(context_file_path, new_text)
-
-        return Response({'message': 'Document uploaded and text extracted successfully.'})
-    except Exception as e:
-        return Response({'message': 'Error uploading document: {}'.format(str(e))})
-
-# Api associated with receiving the url ,parse the content and update the context.
-
-@api_view(['POST'])
-def enter_url(request):
-    url = request.data.get('url')
-    if not url:
-        return HttpResponseBadRequest("Invalid 'url' parameter.")
-
-    try:
-        # Extract the main text content from the URL
-        new_text = extract_main_content(url)
-        # Extract a specific amount of text
-        clean_text=clean_content(new_text)
-
-        # Limit the size of the content to 1000 words.
-        word_limit = 1000
-        limited_text = ' '.join(clean_text.split()[:word_limit])
-
-        # Save the truncated text content to the context file
-        write_context(context_file_path, limited_text)
-
-        return Response({'message': 'URL content saved successfully.'})
-    except Exception as e:
-        return Response({'message': 'Error saving URL content: {}'.format(str(e))})
-
-# function for parsing the url and retrieve the main content
-
-def extract_main_content(url):
-    # Initialize the Article object
-    article = newspaper.Article(url)
-
-    # Download and parse the article
-    article.download()
-    article.parse()
-
-    # Extract the main text content
-    main_text = article.text
-    print(main_text)
-    return (article.text)
-
-
 # Data Preprocessing for the documents and links:
 
 def clean_content(content):
@@ -243,3 +123,118 @@ def remove_non_english_words(text):
         return ' '.join(english_words)
     except LangDetectException:
         return text
+
+
+# API for Bot Interaction as chat
+@api_view(['POST'])
+def chatbot(request):
+    user_message = request.data.get('message')
+    if not user_message:
+        return HttpResponseBadRequest("Invalid 'message' parameter.")
+
+    bot_response = get_bot_response(user_message)
+
+    if not bot_response.strip():
+        bot_response = "Sorry, I don't know the answer to that."
+
+    return Response({'message': bot_response})
+
+
+# API for retrieving the current context
+@api_view(['GET'])
+def get_current_context(request):
+    # Retrieve the current context data from the file
+    context_data = read_context(context_file_path)
+
+    # Format the context data as a JSON response
+    return JsonResponse({'context': context_data})
+
+
+# API associated with updating the current context when user enters the content in the paragraph section to the frontend
+@api_view(['POST'])
+def update_context(request):
+    paragraph = request.data.get('paragraph')
+    if not paragraph:
+        return HttpResponseBadRequest("Invalid 'paragraph' parameter.")
+
+    try:
+        # Update the context file with the paragraph
+        write_context(context_file_path, paragraph)
+
+        return Response({'message': 'Context updated successfully.'})
+    except Exception as e:
+        return Response({'message': 'Error updating context: {}'.format(str(e))})
+
+
+# API associated with the Upload files button, returns the content of the document to the frontend
+@api_view(['POST'])
+def upload_document(request):
+    try:
+        # Retrieve the uploaded document from the request
+        document = request.FILES.get('file')
+        if not document:
+            return HttpResponseBadRequest("No document uploaded.")
+
+        # Save the document using Django's default storage
+        file_path = 'chatbot_app/context.txt'
+        with open(file_path, 'wb') as file:
+            for chunk in document.chunks():
+                file.write(chunk)
+
+        # Extract text content based on the file type
+        text = ''
+        if document.name.endswith('.pdf'):
+            text = extract_text_from_pdf(file_path)
+        elif document.name.endswith('.txt'):
+            text = read_text_file(file_path, encoding='utf-8')  # Specify the appropriate encoding
+        elif document.name.endswith('.docx'):
+            text = extract_text_from_docx(file_path)
+
+        # Data pre-processing for the documents
+        new_text = clean_content(text)
+        # Save the extracted text to the context file
+        write_context(context_file_path, new_text)
+
+        return Response({'message': 'Document uploaded and text extracted successfully.'})
+    except Exception as e:
+        return Response({'message': 'Error uploading document: {}'.format(str(e))})
+
+
+# API associated with receiving the URL, parse the content, and update the context
+@api_view(['POST'])
+def enter_url(request):
+    url = request.data.get('url')
+    if not url:
+        return HttpResponseBadRequest("Invalid 'url' parameter.")
+
+    try:
+        # Extract the main text content from the URL
+        new_text = extract_main_content(url)
+        # Extract a specific amount of text
+        clean_text = clean_content(new_text)
+
+        # Limit the size of the content to 500 words.
+        word_limit = 500
+        limited_text = ' '.join(clean_text.split()[:word_limit])
+
+        # Save the truncated text content to the context file
+        write_context(context_file_path, limited_text)
+
+        return Response({'message': 'URL content saved successfully.'})
+    except Exception as e:
+        return Response({'message': 'Error saving URL content: {}'.format(str(e))})
+
+
+# Function for parsing the URL and retrieve the main content
+def extract_main_content(url):
+    # Initialize the Article object
+    article = newspaper.Article(url)
+
+    # Download and parse the article
+    article.download()
+    article.parse()
+
+    # Extract the main text content
+    main_text = article.text
+    print(main_text)
+    return article.text
